c8ce667709db1f5159d2d8b5092e6627aa3b9389
Renaud <renoruns@gmail.com>
2017-10-21 22:11:19 +0200

    added server-side generated nickname if user do not type one when prompted

--- app.js
+++ app.js
@@ -12,11 +12,22 @@ var io = require("socket.io").listen(server);
 
 io.sockets.on('connection', function(socket, pseudo) {
 
+  //eventHandler lors de la réception d'un nouveau pseudo
   socket.on('nouveau', function(pseudo){
-    socket.pseudo = pseudo;
+    if(pseudo != "") {
+      //attribue le pseudo si l'utilisateur a saisi quelque chose dans le prompt
+      socket.pseudo = pseudo;
+    }else {
+      //Attribue un pseudo aléatoire et l'emet
+      socket.pseudo = "Anon" + Math.floor(Math.random()*100000000);
+      socket.emit('anonUser', socket.pseudo);
+    }
+
+    //prévient les autres utilisateurs de l'arrivée d'un nouveau participant
     socket.broadcast.emit('broadcast', [socket.pseudo + ' a rejoint le chat!']);
   });
 
+  //retransmet les messages reçus aux autres clients
   socket.on('newMessage', function(pseudo, message) {
     socket.broadcast.emit('messageBroadcast', pseudo, message);
   });
